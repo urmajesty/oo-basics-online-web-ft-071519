0432c32161086bd6f618617be7123f522db68db1
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-08-05 16:44:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,8 +3,7 @@ def initialize(title)
 end
 end
 
-title= Book.new(title it)
-book.title = title_it
+title= Book.new(title_it)
 book.author = author_it
 book.page_count = page_it
 book.genre = genre_it
